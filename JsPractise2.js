37576cb924358af4766ca95290af07efd18caa52
Denis <denissdudarevs@gmail.com>
2021-07-15 18:22:44 +0100

    How to do it?

---
+++ JsPractise2.js
@@ -0,0 +1,33 @@
+"use strict";
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// Код возьмите из предыдущего домашнего задания
+const numberOfFilms = +prompt("How many movies did you wached?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const   a = prompt("Last movie you wached?", ""),
+        b = prompt("How much do you rate it?", ""),
+        c = prompt("Last movie you wached?", ""),
+        d = prompt("How much do you rate it?", "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
